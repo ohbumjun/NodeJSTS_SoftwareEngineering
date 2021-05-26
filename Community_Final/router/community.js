2c3989f9ee8562503248913a0c3f1cc15bf1b65b
OH BUM JUN <beomjun0638@hanmail.net>
2021-05-27 00:35:58 +0900

    [Feat] Community Main Page DB Content Display
    with EJS template,
    the DB Contents are displayed in the main page

--- Community_Final/router/community.js
+++ Community_Final/router/community.js
@@ -18,7 +18,8 @@ router.get('/', function(req,res){
             return res.render('index.ejs')
         }
         review_post = result
-        console.log(review_post)
+        console.log("-----------------------")
+        console.log("후기 게시판",review_post)
         var prepare_name = '준비'
         connection.db.query( 'select * from (select * from post where post_type = ? ORDER BY views DESC LIMIT 3)displayprepare ',prepare_name, async (error, result) => {
             if(error){
@@ -30,7 +31,8 @@ router.get('/', function(req,res){
                 return res.render('index.ejs')
             }
             prepare_post =result
-            console.log(prepare_post)
+            console.log("-----------------------")
+            console.log("후기 게시판",prepare_post)
             var free_name = '자유'
             connection.db.query( 'select * from (select * from post where post_type = ? ORDER BY views DESC LIMIT 3)displayreview ',free_name, async (error, result) => {
                 if(error){
@@ -42,7 +44,8 @@ router.get('/', function(req,res){
                     return res.render('index.ejs')
                 }
                 free_post = result
-                console.log(result)
+                console.log("-----------------------")
+                console.log("자유 게시판",result)
                 return res.render('index.ejs',{review:review_post,prepare:prepare_post,free:free_post})
             })
         })
@@ -53,21 +56,34 @@ router.get('/', function(req,res){
 router.get('/feedbackboard', function(req,res){
     return res.render('feedbackboard.ejs')
 })
+// 후기 게시판 -- 개별 게시물 
+router.get('/freeboard/post/:postId', function(req,res){
+    return res.render('freeboard.ejs')
+})
+
 // 준비 게시판
 router.get('/prepareboard', function(req,res){
     return res.render('prepareboard.ejs')
 })
+// 준비 게시판 -- 개별 게시물 
+router.get('/freeboard/post/:postId', function(req,res){
+    return res.render('freeboard.ejs')
+})
+
 // 자유 게시판 
 router.get('/freeboard', function(req,res){
     return res.render('freeboard.ejs')
 })
+// 자유 게시판  -- 개별 게시물 
+router.get('/freeboard/post/:postId', function(req,res){
+    return res.render('freeboard.ejs')
+})
+
 
 // 마이페이지 프로그램 삭제
 router.post('/myprogDelete', function(req,res){
-    
     const ProgNum = req.body.Prog_Num
     const UserId  = req.user.Id
-
     connection.db_rest.query(`DELETE FROM projectparticipate
         WHERE 
         Client_Id = ? and
