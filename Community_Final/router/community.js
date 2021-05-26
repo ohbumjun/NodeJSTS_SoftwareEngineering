ca469a8ecf633e2498c26ab40d75df8c0ba433d0
OH BUM JUN <beomjun0638@hanmail.net>
2021-05-26 23:53:47 +0900

    [Feat] ejs file routing set
    ex) href = "index.html" ==> href = "/"

--- Community_Final/router/community.js
+++ Community_Final/router/community.js
@@ -3,24 +3,22 @@ const router = express.Router();
 const path = require('path');
 const connection = require('../db/db.js');
 
-
-// 이야기
+// 메인 페이지 
 router.get('/', function(req,res){
-    /*
-    connection.db.query( 'SELECT * FROM post ', async (error, result) => {
-        if(error){
-            console.log(error);
-        }
-        // 해당 값이 없다면 : 
-        if( !result ){
-            // return res.render(`index.ejs`,{ data : [] });
-            return res.render('index.ejs')
-        }
-    })
-    */
-    return res.render('index.ejs')
+        return res.render('index.ejs')
 })
-
+// 후기 게시판
+router.get('/feedbackboard', function(req,res){
+    return res.render('feedbackboard.ejs')
+})
+// 준비 게시판
+router.get('/prepareboard', function(req,res){
+    return res.render('prepareboard.ejs')
+})
+// 자유 게시판 
+router.get('/freeboard', function(req,res){
+    return res.render('freeboard.ejs')
+})
 
 // 마이페이지 프로그램 삭제
 router.post('/myprogDelete', function(req,res){
